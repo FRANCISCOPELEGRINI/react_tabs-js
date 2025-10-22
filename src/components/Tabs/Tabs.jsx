d0a7dadf3cd200794475481c4dd62d06fe053515
FRANCISCOPELEGRINI <franciscoprcunha@gmail.com>
2025-10-22 18:35:27 -0300

    MATE ACADEMY REACT JSX TABS

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,11 @@
-export const Tabs = () => {};
+import React, { useState } from 'react';
+
+export const Tabs = ({ activeTabId }) => {
+  const [tab, settab] = useState('');
+
+  if (activeTabId === '') {
+    settab(1);
+  }
+
+  return <h1>Selected tab is tab {tab}</h1>;
+};
